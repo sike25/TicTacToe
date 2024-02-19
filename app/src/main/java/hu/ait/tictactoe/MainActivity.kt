6f474fb94753d8070e2b9ca0ca5c972fef156d67
sike25 <oogieva25@amherst.edu>
2024-02-19 12:19:47 +0100

    Initial commit

---
+++ app/src/main/java/hu/ait/tictactoe/MainActivity.kt
@@ -0,0 +1,19 @@
+package hu.ait.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.ait.tictactoe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClear.setOnClickListener {
+            binding.ticTacToeView.resetGame()
+        }
+    }
+}
